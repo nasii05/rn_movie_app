d823514112f92b9401db54d8768f1dbb4ae6ad2f
nasii05 <iamnasih@gmail.com>
2025-04-29 23:13:36 +0400

    search api implementation

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,22 +3,24 @@ import { icons } from '@/constants/icons';
 
 interface Props {
     placeholder: string;
+    value?: string;
     onPress?: () => void;
+    onChangeText?: (text:string) => void;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, value, onChangeText, onPress}: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200
      rounded-full px-5 py-4'>
-      <Image source={icons.search}  className='size-5'
+      <Image source={icons.search}  className='w-5 h-5'
       resizeMode='contain' tintColor="#ab8bff"/>
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=''
-        onChange={()=>{}}
-        placeholderTextColor="#a8b5db"
+        value={value}
+        onChangeText={onChangeText}
         className='flex-1 ml-2 text-white'
+        placeholderTextColor="#a8b5db"
       />
     </View>
   )
