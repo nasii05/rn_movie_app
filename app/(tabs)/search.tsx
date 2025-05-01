dd295c5a81805ff981b7b1a5c382586cf8a88aea
nasii05 <iamnasih@gmail.com>
2025-05-01 14:14:06 +0400

    trending movies api fix

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -26,15 +26,19 @@ const Search = () => {
         const timoutId = setTimeout(async () => {
             if(searchQuery.trim()){
                 await loadMovies();
-                if(movies?.length > 0 && movies?.[0])
-                await updateSearchCount(searchQuery, movies[0]);
             }else{
                 reset()
             }
         }, 500)
 
         return () => clearTimeout(timoutId);
-    },[searchQuery])
+    },[searchQuery]);
+
+    useEffect(() => {
+        if(movies?.length > 0 && movies?.[0]){
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
 
     return (
         <View className="flex-1 bg-primary">
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -35,7 +35,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
                 movie_id: movie.id,
                 count: 1,
                 title: movie.title,
-                poster_url: `https://image.tmdb/t/p/w500${movie.poster_path}`
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
             })
         }
         
@@ -45,10 +45,22 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
         throw error;
     }
 
-   
-
 
     // check if a record of that search had already been stored
     // if a documenr is found increment the searchCount field
     // if no document if found create a Appwrite database -> 1
+}
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ]);
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error){
+        console.log(error);
+        return undefined;
+    }
 }
